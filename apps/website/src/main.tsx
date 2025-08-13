fd27a4c09622691653f3e601f4648f8cf1c2aada
Guy Mograbi <guy.mograbi@gmail.com>
2025-08-12 19:42:19 -0700

    first commit

---
+++ apps/website/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Button } from '@devland/ui-kit';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Button />
+  </React.StrictMode>
+);
